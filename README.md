b591beaebacf174fdd47f535d62af45a223a8f4a
omarvaldez5 <ovaldez@tuta.io>
2022-10-12 20:45:34 -0600

    adding simple function script

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# practice
+# Practice
 Code &amp; stuff
---
+++ simple_function.py
@@ -0,0 +1,50 @@
+# ============================================================================ #
+# START
+# ============================================================================ #
+
+# Function
+def two_plus_numbers() -> int:
+    """This is a simple function to return the positive sum of two numbers
+
+    Returns:
+        int: Positive integer
+    """
+
+    try:
+        # Description
+        print("Welcome! Please, Add two numbers that end up being a positive reuslt\n")
+
+        # Inputs
+        param1 = int(input("Enter the first positive number: "))
+        param2 = int(input("Enter the second positive number: "))
+
+        # Calculation
+        sum_numbers = param1 + param2
+
+        # Constraints
+        if sum_numbers > 0:
+            print("\nGreat! Your result is the following: ")
+        else:
+            raise Exception(
+                f"You total is {sum_numbers}, which is not a positive number. Please try again.")
+
+        return sum_numbers
+
+    except ValueError as e:
+        print("Remember, you are supposed to enter a positive number! Error: ", e)
+        raise
+
+
+# ============================================================================ #
+# Print Section
+# ============================================================================ #
+print("\n")
+print("*" * 30)
+print(two_plus_numbers())
+print("*" * 30)
+print("\n")
+
+
+# ============================================================================ #
+# END
+# ============================================================================ #
